87727293170dab9c066e38919d75ad75caa65a0f
sstulin <ss8125@nyu.edu>
2016-10-11 13:14:44 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,12 +4,12 @@ var b = 375;
 //var yspeed = 3;
 
 function setup() {
-  createCanvas(1334,750);
+  createCanvas(750,1334);
   
   for (var x = 0; x<1000; x=x+40){
       fill(255,20,50);
       noStroke();
-      rect(x,random(0,750), random(10,150), random(10,150));
+      rect(random(0,750), x , random(10,150), random(10,150));
     
   
 
@@ -21,23 +21,6 @@ function draw() {
   a=a+accelerationX
   b=b+accelerationY
   fill(0,0,255);
-  ellipse(a,b,20,20);
+  ellipse(b,a,20,20);
   
     }
-  //        background (255);rect
-  //        stroke (0);
-   //       strokeWeight(4);
-   //       noFill();
-    //      ellipse(x,y,100,100);
-    //      x=x+xspeed;
-    //      y=y+yspeed;
-     //     if (x>width)
-    //    {xspeed = -xspeed;}
-    //  if (x<0)
-     //   {xspeed = -xspeed;}
-        
-   //     if (y>height)
-   //     {yspeed = -yspeed;}
-  //    if (y<0)
- //       {yspeed = -yspeed;}
-  
